e5301e0d0a1a9186fb74951c52f1c9e30b45b0a2
Akari-Glitch <cuartetodeperturbados@gmail.com>
2022-11-19 10:53:23 -0400

    cambios

--- tarea.js
+++ tarea.js
@@ -11,13 +11,13 @@ scene("game", () => {
   const PIPE_GAP = 120;
   function producePipes() {
     const offset = rand(-64, 64);
-    add([sprite("pipe"), pos(width(), 0), "pipe", scale(7), area()]);
+    add([sprite("pipe"), pos(width(), 0), "pipe", scale(9), area()]);
 
     add([
       sprite("pipe", { flipY: true }),
       pos(width(), height()),
       origin("botleft"),
-      scale(7),
+      scale(9),
       "pipe",
       area(),
     ]);
